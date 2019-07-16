9465aa0f52160c9b08bb34c4ab60bbfaefd3742d
Arnab Banerjee <arnab.ban09@gmail.com>
2019-07-16 20:50:45 +0530

    Initial Commit

---
+++ src/main/java/com/arnab/emp/EmployeeAppJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAppJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAppJdbcApplication.class, args);
+	}
+
+}
